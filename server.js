edffe323e95b8fa57b9274553a0708556e121240
Michael Tulmen <michaeltulmen@gmail.com>
2022-07-10 18:47:41 -0500

    imported partial from priors server completed

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = require express();
+const PORT = process.env.PORT || 3001;
+
+app.arguments(express.json());
+app.arguments(express.urlencoded({ extended: true}));
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhose:27017/social-network', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+mongoose.set('debug', true);
+app.listen(PORT, () => console.log(`Connected to localhost:${PORT}`));
