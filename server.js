a2ca37a029263003eaec488c6e4b40e0f561eda1
Michael Tulmen <michaeltulmen@gmail.com>
2022-07-10 18:48:27 -0500

    fixed

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = require express();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.arguments(express.json());
-app.arguments(express.urlencoded({ extended: true}));
-app.use(require('./routes'));
+app.arguments(express.urlencoded({ extended: true }));
+app.use(require("./routes"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhose:27017/social-network', {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhose:27017/social-network",
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-mongoose.set('debug', true);
+    useUnifiedTopology: true,
+  }
+);
+mongoose.set("debug", true);
 app.listen(PORT, () => console.log(`Connected to localhost:${PORT}`));
